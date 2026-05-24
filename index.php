a09ea87861a81420ec4998aaa7d23a197f465f75
Ravindi <ravindisandali5387@gmail.com>
2026-05-24 14:52:09 +0530

    I Created my blog app

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog App</title>
+    <link rel="stylesheet" href="plugin/bootstrap.min.css">
+    <script src="plugin/jquery.min.js"></script>
+    <script src="plugin/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+  
+    <?php include 'inc/navbar.php'; ?>
+
+    <div class="container">
+    <div class="row">
+        <div class="col-md-12">
+
+            <div class="p-5 mb-4 bg-light rounded-3">
+                <div class="container-fluid py-5">
+                    <h1 class="display-5 fw-bold">Welcome</h1>
+                    <p class="fs-4">My blog application</p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+</body>
+</html> 
